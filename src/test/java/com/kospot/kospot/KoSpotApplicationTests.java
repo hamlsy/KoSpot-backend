752e21c3a57797c0ee8dc2fd5aad5807a118d3ff
hamlsy <dltmddud1122@naver.com>
2025-01-12 13:40:32 +0900

    init: initialize-project #1

---
+++ src/test/java/com/kospot/kospot/KoSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kospot.kospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
