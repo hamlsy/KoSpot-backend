cf944a774424716f9f6f938b2fce08271b593214
hamlsy <dltmddud1122@naver.com>
2025-01-20 14:13:49 +0900

    feat: Add Platinum Level #11

--- src/main/java/com/kospot/kospot/domain/rank/entity/RankType.java
+++ src/main/java/com/kospot/kospot/domain/rank/entity/RankType.java
@@ -1,13 +1,14 @@
 package com.kospot.kospot.domain.rank.entity;
 
 public enum RankType {
-    BRONZE, SILVER, GOLD, DIAMOND, MASTER;
+    BRONZE, SILVER, GOLD, PLATINUM,DIAMOND, MASTER;
 
     public static RankType getRankByRating(int rating) {
         if (rating < 500) return BRONZE;
         if (rating < 1000) return SILVER;
         if (rating < 1500) return GOLD;
-        if (rating < 2000) return DIAMOND;
+        if (rating < 2000) return PLATINUM;
+        if (rating < 2500) return DIAMOND;
         return MASTER;
     }
 }
