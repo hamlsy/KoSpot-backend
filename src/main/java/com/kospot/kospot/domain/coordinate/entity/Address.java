72f1a7455710accab5c41d84c4e41553bd6f333c
hamlsy <dltmddud1122@naver.com>
2025-02-03 14:30:03 +0900

    feat: Add Enumerated in Address Class #13

--- src/main/java/com/kospot/kospot/domain/coordinate/entity/Address.java
+++ src/main/java/com/kospot/kospot/domain/coordinate/entity/Address.java
@@ -1,6 +1,11 @@
 package com.kospot.kospot.domain.coordinate.entity;
 
+import com.kospot.kospot.domain.coordinate.entity.region.eupmyeondong.Eupmyeondong;
+import com.kospot.kospot.domain.coordinate.entity.region.sido.Sido;
+import com.kospot.kospot.domain.coordinate.entity.region.sigungu.Sigungu;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +15,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
-    private String sido;     // 시도 (서울특별시, 경기도 등)
-    private String sigungu;  // 시군구 (강남구, 수원시 등)
-    private String eupmyeondong; // 읍면동/구 (삼성동, 영등포동 등)
+
+    @Enumerated(EnumType.STRING)
+    private Sido sido;     // 시도 (서울특별시, 경기도 등)
+
+    @Enumerated(EnumType.STRING)
+    private Sigungu sigungu;  // 시군구 (강남구, 수원시 등)
+
+    @Enumerated(EnumType.STRING)
+    private Eupmyeondong eupmyeondong; // 읍면동/구 (삼성동, 영등포동 등)
+
     private String eupmyeonri;   // 읍/면/리 (농촌 지역 주소)
 
 }
--- src/main/java/com/kospot/kospot/domain/coordinate/repository/CoordinateRepository.java
+++ src/main/java/com/kospot/kospot/domain/coordinate/repository/CoordinateRepository.java
@@ -1,10 +1,11 @@
 package com.kospot.kospot.domain.coordinate.repository;
 
 import com.kospot.kospot.domain.coordinate.entity.Coordinate;
+import com.kospot.kospot.domain.coordinate.entity.region.sido.Sido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CoordinateRepository extends JpaRepository<Coordinate, Long> {
-    List<Coordinate> findByAddress_Sido(String sido);
+    List<Coordinate> findByAddress_Sido(Sido sido);
 }
