dac224b9b5096316ec0c63654b8499184fd3d631
hamlsy <dltmddud1122@naver.com>
2025-02-11 20:11:30 +0900

    remove: Remove Sigungu #21

--- src/main/java/com/kospot/kospot/domain/coordinate/entity/Address.java
+++ src/main/java/com/kospot/kospot/domain/coordinate/entity/Address.java
@@ -1,8 +1,6 @@
 package com.kospot.kospot.domain.coordinate.entity;
 
-import com.kospot.kospot.domain.coordinate.entity.sigungu.converter.SigunguConverter;
 import com.kospot.kospot.domain.coordinate.entity.sido.Sido;
-import com.kospot.kospot.domain.coordinate.entity.sigungu.Sigungu;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,8 +18,7 @@ public class Address {
     @Enumerated(EnumType.STRING)
     private Sido sido;     // 시도 (서울특별시, 경기도 등)
 
-    @Convert(converter = SigunguConverter.class)
-    private Sigungu sigungu;  // 시군구 (강남구, 수원시 등)
+    private String sigungu;  // 시군구 (강남구, 수원시 등)
 
 //    @Convert(converter = EupmyeondongConverter.class)
 //    private Eupmyeondong eupmyeondong; // 읍면동/구 (삼성동, 영등포동 등)
