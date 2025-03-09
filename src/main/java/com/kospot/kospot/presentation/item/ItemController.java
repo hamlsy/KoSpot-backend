816d2582563bc93194af5a1dfe49493e5e9f36d6
hamlsy <dltmddud1122@naver.com>
2025-03-09 18:12:59 +0900

    feat: add basic item controller template #37

--- src/main/java/com/kospot/kospot/presentation/item/ItemController.java
+++ src/main/java/com/kospot/kospot/presentation/item/ItemController.java
@@ -1,16 +1,16 @@
 package com.kospot.kospot.presentation.item;
 
 import com.kospot.kospot.application.item.FindItemsByTypeUseCase;
+import com.kospot.kospot.domain.item.dto.response.ItemResponse;
 import com.kospot.kospot.exception.payload.dto.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,15 +24,31 @@ public class ItemController {
 
     @Operation(summary = "아이템 타입 조회", description = "타입 별 아이템들을 조회합니다.")
     @GetMapping("/{itemTypeKey}")
-    public ApiResponseDto<?> findItemsByItemType(@PathVariable("itemTypeKey") String itemTypeKey){
+    public ApiResponseDto<List<ItemResponse.ItemDto>> findItemsByItemType(@PathVariable("itemTypeKey") String itemTypeKey){
         return ApiResponseDto.onSuccess(findItemsByTypeUseCase.execute(itemTypeKey));
     }
 
-    //todo register item - S3
+    //todo register item - S3, admin
+    @Operation(summary = "아이템 등록", description = "아이템을 등록합니다.")
+    public ApiResponseDto<?> registerItem(){
+        return null;
+    }
 
-    //todo delete item
+
+    //todo delete item, admin
+    @Operation(summary = "아이템 삭제", description = "아이템을 삭제합니다.")
+    @DeleteMapping("/{id}")
+    public ApiResponseDto<?> deleteItem(@PathVariable("id") Long itemId){
+        return null;
+    }
 
     //todo update item
+
+    @Operation(summary = "아이템 업데이트", description = "아이템을 업데이트 합니다.")
+    @PutMapping("/{id}")
+    public ApiResponseDto<?> updateItem(@PathVariable("id") Long itemId){
+        return null;
+    }
 
 
 }
