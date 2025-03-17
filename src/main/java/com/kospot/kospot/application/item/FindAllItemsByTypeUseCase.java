b66a5c28bcfea3e1c7b8bdaacca54201caca63c5
hamlsy <dltmddud1122@naver.com>
2025-03-17 22:23:28 +0900

    refactor: change find all items usecase method #39

--- src/main/java/com/kospot/kospot/application/item/FindAllItemsByTypeUseCase.java
+++ src/main/java/com/kospot/kospot/application/item/FindAllItemsByTypeUseCase.java
@@ -3,13 +3,11 @@ package com.kospot.kospot.application.item;
 import com.kospot.kospot.domain.item.adaptor.ItemAdaptor;
 import com.kospot.kospot.domain.member.entity.Member;
 import com.kospot.kospot.presentation.item.dto.response.ItemResponse;
-import com.kospot.kospot.domain.item.entity.Item;
 import com.kospot.kospot.domain.item.entity.ItemType;
 import com.kospot.kospot.global.annotation.usecase.UseCase;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @UseCase
 @RequiredArgsConstructor
@@ -17,15 +15,7 @@ public class FindAllItemsByTypeUseCase {
 
     private final ItemAdaptor itemAdaptor;
 
-    public List<ItemResponse> execute(String itemTypeKey) {
-        ItemType itemType = ItemType.fromKey(itemTypeKey);
-        List<Item> items = itemAdaptor.queryAvailableItemsByItemTypeFetchImage(itemType);
-        return items.stream()
-                .map(ItemResponse::from)
-                .collect(Collectors.toList());
-    }
-
-    public List<ItemResponse> executeV2(Member member, String itemTypeKey){
+    public List<ItemResponse> execute(Member member, String itemTypeKey) {
         ItemType itemType = ItemType.fromKey(itemTypeKey);
         return itemAdaptor.queryAvailableItemsByWithOwnersByFetchImage(member, itemType);
     }
