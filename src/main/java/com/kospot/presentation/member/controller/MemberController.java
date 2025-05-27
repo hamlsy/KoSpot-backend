ad251617c6f48ea520c64529d06665beb2f02012
hamlsy <dltmddud1122@naver.com>
2025-05-27 13:15:25 +0900

    feat: add find test member api #56

--- src/main/java/com/kospot/presentation/member/controller/MemberController.java
+++ src/main/java/com/kospot/presentation/member/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.kospot.presentation.member.controller;
 
 import com.kospot.application.member.TestTempLoginUseCase;
+import com.kospot.domain.member.entity.Member;
 import com.kospot.global.exception.payload.dto.ApiResponseDto;
+import com.kospot.infrastructure.security.aop.CurrentMember;
 import com.kospot.infrastructure.security.dto.JwtToken;
 import com.kospot.infrastructure.security.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,9 +30,16 @@ public class MemberController {
     /**
      *  Test
      */
+    @Operation(summary = "테스트용 임시 로그인", description = "테스트용 임시 로그인")
     @GetMapping("/tempLogin/{username}")
     public ApiResponseDto<JwtToken> tempLogin(@PathVariable("username") String username) {
         return ApiResponseDto.onSuccess(testTempLoginUseCase.testLogin(username));
+    }
+
+    @Operation(summary = "테스트용 멤버 조회", description = "테스트용 멤버 조회")
+    @GetMapping("/me")
+    public ApiResponseDto<Member> tempLogin(@CurrentMember Member member) {
+        return ApiResponseDto.onSuccess(member);
     }
 
 }
