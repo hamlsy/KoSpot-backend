4050a5c96cca58207b3ca8d6e5438d2367d80c4c
hamlsy <dltmddud1122@naver.com>
2025-09-12 14:01:16 +0900

    feat: add TimerData VO #80

---
+++ src/main/java/com/kospot/domain/multigame/timer/vo/TimerData.java
@@ -0,0 +1,22 @@
+package com.kospot.domain.multigame.timer.vo;
+
+import com.kospot.domain.game.vo.GameMode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimerData {
+
+    private String gameId;
+    private String roundId;
+    private GameMode gameMode;
+    private int durationSeconds;
+    private long startTimeMillis;
+    private List<String> playerIds;
+
+}
