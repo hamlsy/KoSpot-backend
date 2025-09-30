cb1191e2fbd2c6537da6a87d6830af3fd7df28fd
hamlsy <dltmddud1122@naver.com>
2025-09-30 18:19:03 +0900

    feat: implement startRoundTimer method in GameTimerService, add comment todo list #80

--- src/main/java/com/kospot/infrastructure/websocket/domain/gameTimer/service/GameTimerService.java
+++ src/main/java/com/kospot/infrastructure/websocket/domain/gameTimer/service/GameTimerService.java
@@ -1,16 +1,21 @@
 package com.kospot.infrastructure.websocket.domain.gameTimer.service;
 
 import com.kospot.application.multiplayer.timer.message.TimerStartMessage;
+import com.kospot.application.multiplayer.timer.message.TimerSyncMessage;
 import com.kospot.domain.game.vo.GameMode;
 import com.kospot.infrastructure.redis.domain.timer.dao.GameTimerRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ScheduledFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +24,16 @@ public class GameTimerService {
     private final GameTimerRedisRepository gameTimerRedisRepository;
     private final SimpMessagingTemplate messagingTemplate;
     private final ApplicationEventPublisher eventPublisher;
+    private final TaskScheduler taskScheduler;
 
     private static final int SYNC_INTERVAL_MS = 5000; // 5초마다 동기화
     private static final int FINAL_COUNTDOWN_THRESHOLD_MS = 10000; // 마지막 10초 카운트다운
 
     private static final String TIMER_START_SUFFIX = "/tier/start";
+
+    private String getTimerTopic(String gameId) {
+        return String.format("/game/%s/timer/sync", gameId);
+    }
 
     /**
      * 라운드 시작
@@ -41,10 +51,10 @@ public class GameTimerService {
                 .serverTimestamp(System.currentTimeMillis())
                 .build();
 
-         broadcastToGame(gameId, TIMER_START_SUFFIX, message);
+        broadcastToGame(gameId, TIMER_START_SUFFIX, message);
 
         // 타이머 스케줄링
-        // scheduleTimerSync(round);
+        // scheduleTimerSync(gameId);
         // scheduleRondCompletion(round);
 
     }
@@ -53,5 +63,38 @@ public class GameTimerService {
         String destination = "/topic/game/" + gameId + suffix;
         messagingTemplate.convertAndSend(destination, message);
     }
+
+    private void scheduleTimerSync(String gameId) {
+        String topic = getTimerTopic(gameId);
+
+        ScheduledFuture<?> syncTask = taskScheduler.scheduleAtFixedRate(() -> {
+            //todo implement timerSyncMessage
+            // params: roundId, remainingTimeMs, serverTimestamp, isFinalCountDown
+            // redis 에서 roundId로 round 정보 가져오기
+
+            TimerSyncMessage message = TimerSyncMessage.builder()
+                    .roundId()
+                    .serverTimestamp()
+                    .remainingTimeMs()
+                    .isFinalCountDown()
+                    .build();
+            broadcastToGame(gameId, topic, message);
+        }, Instant.now().plusMillis(SYNC_INTERVAL_MS), Duration.ofMillis(SYNC_INTERVAL_MS)); // 현재 시각 기준으로 5초 뒤에 첫 실행, 이후 5초 간격으로 계속 반복
+
+    }
+
+    /**
+     * 마지막 10초 고빈도 동기화 (1초마다)
+     */
+    private void scheduleFrequentSync() {
+        //todo implement
+    }
+
+    /**
+     * 라운드 종료 스케줄링
+     */
+    private void scheduleRoundCompletion() {
+        //todo implement
+    }}
 
 }
