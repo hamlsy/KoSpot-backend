09f9d583ac69021b9a0b32e10bf0364e3e9a29e3
hamlsy <dltmddud1122@naver.com>
2025-11-12 21:31:45 +0900

    feat: implement check access service #126

--- src/main/java/com/kospot/presentation/multi/access/dto/response/GameAccessResponse.java
+++ src/main/java/com/kospot/presentation/multi/access/dto/response/GameAccessResponse.java
@@ -1,5 +1,6 @@
 package com.kospot.presentation.multi.access.dto.response;
 
+import com.kospot.presentation.multi.gameroom.dto.response.GameRoomDetailResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,14 +14,19 @@ public class GameAccessResponse {
     private boolean allowed;              // 접근 가능 여부
     private String message;                // 접근 불가 시 메시지
 //    private GameState gameState;          // 게임 상태 (WAITING, IN_PROGRESS, FINISHED)
-    private GameInfo gameInfo;             // 게임 정보 (접근 가능한 경우)
+    private GameRoomDetailResponse gameRoomDetailResponse;
 
-    @Data
-    @Builder
-    public static class GameInfo {
-        private Long gameId;
-        private String roomId;
-//        private List<PlayerInfo> players;  // 플레이어 정보 포함
-//        private GameSettings settings;
+    public static GameAccessResponse notAllowed(String message) {
+        return GameAccessResponse.builder()
+                .allowed(false)
+                .message(message)
+                .build();
+    }
+
+    public static GameAccessResponse allowed(GameRoomDetailResponse gameRoomDetailResponse) {
+        return GameAccessResponse.builder()
+                .allowed(true)
+                .gameRoomDetailResponse(gameRoomDetailResponse)
+                .build();
     }
 }
